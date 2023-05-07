739d7001b22fae5edcd2752e408ab6fb65e7508b
unknown <kireSuomi@gmail.com>
2023-05-07 19:15:54 +0200

    Make app into class

--- public/assets/js/index.mjs
+++ public/assets/js/index.mjs
@@ -2,70 +2,98 @@ import getMostCommonLetter from "./getMostCommonLetter.mjs";
 import getDominantCategory from "./getDominantCategory.mjs";
 import letterOccurence from "./letterOccurence.mjs";
 
-const btnGetJokes = document.getElementById("getJokes");
+class Application {
+  constructor() {
+    this.button = document.getElementById("getJokes");
+    this.button.addEventListener("click", () => {
+      this.getJokes();
+    });
 
-btnGetJokes.addEventListener("click", getJokes);
-
-function getJokeMarkupEl(joke) {
-  const jokeDiv = document.createElement("div");
-
-  if (joke.type === "twopart") {
-    jokeDiv.innerHTML = `
-      <h3>${joke.type} - ${joke.category}</h3>
-      <p>${joke.setup}</p>
-      <p>${joke.delivery}</p>
-    `;
-  } else {
-    jokeDiv.innerHTML = `
-      <h3>${joke.type} - ${joke.category}</h3>
-      <p>${joke.joke}</p>
-    `;
+    this.jokes = [];
   }
 
-  jokeDiv.innerHTML = jokeDiv.innerHTML.replaceAll(
-    /(\w*a\w*)/gi,
-    '<span style="color: blue;">$1</span>'
-  );
-  return jokeDiv;
-}
+  getJokes() {
+    const amount = document.getElementById("amount").value ?? undefined;
+    const type = document.getElementById("type").value ?? undefined;
 
-function printJokes(jokes) {
-  const jokesDiv = document.getElementById("jokes");
-  jokesDiv.innerHTML = "";
+    fetch(`/api/jokes?amount=${amount}&type=${type}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        if (data.error) {
+          alert(data.error);
+        } else {
+          this.jokes = data;
 
-  jokes.forEach((joke) => {
-    jokesDiv.appendChild(getJokeMarkupEl(joke));
-  });
-}
+          this.printJokes();
+          this.printStatistics();
+        }
+      });
+  }
 
-function printStatistics(jokes) {
-  const singleJokes = jokes.filter((joke) => joke.type === "single");
-  const twoPartJokes = jokes.filter((joke) => joke.type === "twopart");
+  printJokes() {
+    const jokesDiv = document.getElementById("jokes");
+    jokesDiv.innerHTML = "";
 
-  //Single part jokes ========
-  let singleSum = "";
-  const totalCharsSingleJokes = singleJokes.reduce((acc, joke) => {
-    singleSum += joke.joke;
-    return acc + joke.joke.length;
-  }, 0);
+    this.jokes.forEach((joke) => {
+      jokesDiv.appendChild(this.getJokeMarkupEl(joke));
+    });
+  }
 
-  if (singleJokes.length > 0) {
-    const singleThirdLetter = singleJokes
-      .reverse()[0]
-      .joke.split(" ")
-      .reverse(0)[0]
-      .split("")[2]
-      .toLocaleLowerCase();
+  getJokeMarkupEl(joke) {
+    const jokeDiv = document.createElement("div");
 
-    const singleThirdLetterOccurences = letterOccurence(
-      singleSum,
-      singleThirdLetter
+    if (joke.type === "twopart") {
+      jokeDiv.innerHTML = `
+        <h3>${joke.type} - ${joke.category}</h3>
+        <p>${joke.setup}</p>
+        <p>${joke.delivery}</p>
+      `;
+    } else {
+      jokeDiv.innerHTML = `
+        <h3>${joke.type} - ${joke.category}</h3>
+        <p>${joke.joke}</p>
+      `;
+    }
+
+    jokeDiv.innerHTML = jokeDiv.innerHTML.replaceAll(
+      /(\w*a\w*)/gi,
+      '<span style="color: blue;">$1</span>'
     );
-    const singleMostCommonLetter = getMostCommonLetter(singleSum);
+    return jokeDiv;
+  }
 
-    const singleDominantData = getDominantCategory(singleJokes);
+  printStatistics() {
+    const jokes = this.jokes;
 
-    document.getElementById("statistics").innerHTML = `
+    const singleJokes = jokes.filter((joke) => joke.type === "single");
+    const twoPartJokes = jokes.filter((joke) => joke.type === "twopart");
+
+    //Single part jokes ========
+    let singleSum = "";
+    const totalCharsSingleJokes = singleJokes.reduce((acc, joke) => {
+      singleSum += joke.joke;
+      return acc + joke.joke.length;
+    }, 0);
+
+    if (singleJokes.length > 0) {
+      const singleThirdLetter = singleJokes
+        .reverse()[0]
+        .joke.split(" ")
+        .reverse(0)[0]
+        .split("")[2]
+        .toLocaleLowerCase();
+
+      const singleThirdLetterOccurences = letterOccurence(
+        singleSum,
+        singleThirdLetter
+      );
+      const singleMostCommonLetter = getMostCommonLetter(singleSum);
+
+      const singleDominantData = getDominantCategory(singleJokes);
+
+      document.getElementById("statistics").innerHTML = `
       <h2>Statistics single:</h2>
       <p>Total chars: ${totalCharsSingleJokes}</p>
       <p>Third letter of the last joke occurence (${singleThirdLetter}): ${singleThirdLetterOccurences}</p>
@@ -74,36 +102,36 @@ function printStatistics(jokes) {
         singleDominantData.category
       } (${singleDominantData.percentage.toFixed(0)}%)</p>
     `;
-  } else {
-    document.getElementById("statistics").innerHTML = "";
-  }
+    } else {
+      document.getElementById("statistics").innerHTML = "";
+    }
 
-  //Two part jokes ========
+    //Two part jokes ========
 
-  //All chars of the two part jokes
-  if (twoPartJokes.length > 0) {
-    let twoPartSum = "";
-    const totalCharsTwoPartJokes = twoPartJokes.reduce((acc, joke) => {
-      twoPartSum += joke.setup + joke.delivery;
-      return acc + joke.setup.length + joke.delivery.length;
-    }, 0);
+    //All chars of the two part jokes
+    if (twoPartJokes.length > 0) {
+      let twoPartSum = "";
+      const totalCharsTwoPartJokes = twoPartJokes.reduce((acc, joke) => {
+        twoPartSum += joke.setup + joke.delivery;
+        return acc + joke.setup.length + joke.delivery.length;
+      }, 0);
 
-    //third letter of last joke occurenct amount
-    const doubbleThirdLetter = twoPartJokes
-      .reverse()[0]
-      .delivery.split(" ")
-      .reverse(0)[0]
-      .split("")[2]
-      .toLocaleLowerCase();
-    const doubbleThirdLetterOccurences = letterOccurence(
-      twoPartSum,
-      doubbleThirdLetter
-    );
-    const doubbleMostCommonLetter = getMostCommonLetter(twoPartSum);
+      //third letter of last joke occurenct amount
+      const doubbleThirdLetter = twoPartJokes
+        .reverse()[0]
+        .delivery.split(" ")
+        .reverse(0)[0]
+        .split("")[2]
+        .toLocaleLowerCase();
+      const doubbleThirdLetterOccurences = letterOccurence(
+        twoPartSum,
+        doubbleThirdLetter
+      );
+      const doubbleMostCommonLetter = getMostCommonLetter(twoPartSum);
 
-    const doubbleDominantData = getDominantCategory(twoPartJokes);
+      const doubbleDominantData = getDominantCategory(twoPartJokes);
 
-    document.getElementById("statistics_doubble").innerHTML = `
+      document.getElementById("statistics_doubble").innerHTML = `
       <h2>Statistics doubble:</h2>
       <p>Total chars: ${totalCharsTwoPartJokes}</p>
       <p>Third letter of the last joke occurence (${doubbleThirdLetter}): ${doubbleThirdLetterOccurences}</p>
@@ -112,27 +140,10 @@ function printStatistics(jokes) {
         doubbleDominantData.category
       } (${doubbleDominantData.percentage.toFixed(0)}%)</p>
     `;
-  } else {
-    document.getElementById("statistics_doubble").innerHTML = "";
+    } else {
+      document.getElementById("statistics_doubble").innerHTML = "";
+    }
   }
 }
 
-function getJokes() {
-  const amount = document.getElementById("amount").value ?? undefined;
-  const type = document.getElementById("type").value ?? undefined;
-
-  console.log(amount, type);
-  fetch(`/api/jokes?amount=${amount}&type=${type}`)
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      if (data.error) {
-        alert(data.error);
-      } else {
-        //Print the jokes and statistics
-        printJokes(data);
-        printStatistics(data);
-      }
-    });
-}
+const App = new Application();
